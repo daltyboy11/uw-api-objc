8cf17b8237d0d38021409c5be94984f7ad0d2a0c
Dalton Sweeney <daltyboy11@gmail.com>
2018-07-22 12:43:39 -0400

    parking

--- UWaterlooAPIObjc/Endpoints/Parking.h
+++ UWaterlooAPIObjc/Endpoints/Parking.h
@@ -10,4 +10,25 @@
 
 @interface Parking : Endpoint
 
+/** This method fetches real-time parking counts in select parking lots across campus. */
+- (void)parkingLotsCountWithCompletion:(Handler)completion;
+
+/** This method fetches list of metered parking lots across campus. */
+- (void)meteredParkingLotsWithCompletion:(Handler)completion;
+
+/** This method fetches list of permit based parking lots across campus. */
+- (void)permitParkingLotsWithCompletion:(Handler)completion;
+
+/** This method fetches list of visitor parking lots across campus. */
+- (void)visitorParkingLotsWithCompletion:(Handler)completion;
+
+/** This method fetches list of short-term parking lots across campus. */
+- (void)shortTermParkingLotsWithCompletion:(Handler)completion;
+ 
+/** This method fetches list of accessible parking lots across campus. */
+- (void)accessibleParkingLotsWithCompletion:(Handler)completion;
+
+/** This method fetches list of motorcycle allowed parking lots across campus. */
+- (void)motorcycleAllowedParkingLotsWithCompletion:(Handler)completion;
+
 @end
--- UWaterlooAPIObjc/Endpoints/Parking.m
+++ UWaterlooAPIObjc/Endpoints/Parking.m
@@ -10,4 +10,46 @@
 
 @implementation Parking
 
+- (void)parkingLotsCountWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"parking/watpark.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)meteredParkingLotsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"parking/lots/meter.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)permitParkingLotsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"parking/lots/permit.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)visitorParkingLotsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"parking/lots/visitor.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)shortTermParkingLotsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"parking/lots/shortterm.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)accessibleParkingLotsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"parking/lots/accessible.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)motorcycleAllowedParkingLotsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"parking/lots/motorcycle.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
 @end
