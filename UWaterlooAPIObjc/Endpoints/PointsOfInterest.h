c258cdb85de83c094b5e4d0d369dcf9aa235a104
Dalton Sweeney <daltyboy11@gmail.com>
2018-07-22 12:35:01 -0400

    points of interests

--- UWaterlooAPIObjc/Endpoints/PointsOfInterest.h
+++ UWaterlooAPIObjc/Endpoints/PointsOfInterest.h
@@ -10,4 +10,31 @@
 
 @interface PointsOfInterest : Endpoint
 
+/** This method returns list of ATMs across campus. */
+- (void)atmsWithCompletion:(Handler)completion;
+
+/** This method returns list of Greyhound bus stops across city. */
+- (void)greyhoundsWithCompletion:(Handler)completion;
+
+/** This method returns list of emergency helplines across campus. */
+- (void)helplinesWithCompletion:(Handler)completion;
+
+/** This method returns list of libraries across city. */
+- (void)librariesWithCompletion:(Handler)completion;
+
+/** This method returns list of photospheres across campus. */
+- (void)photospheresWithCompletion:(Handler)completion;
+
+/** This method returns list of defibrillators across campus. */
+- (void)defibrillatorsWithCompletion:(Handler)completion;
+
+/** This method returns list of construction sites across campus. */
+- (void)constructionsitesWithCompletion:(Handler)completion;
+
+/** This method returns list of accessible entrances across campus. */
+- (void)accessibleEntrancesWithCompletion:(Handler)completion;
+
+/** This method returns list of visitor information centers across campus. */
+- (void)visitorInformationWithCompletion:(Handler)completion;
+
 @end
--- UWaterlooAPIObjc/Endpoints/PointsOfInterest.m
+++ UWaterlooAPIObjc/Endpoints/PointsOfInterest.m
@@ -10,4 +10,58 @@
 
 @implementation PointsOfInterest
 
+- (void)atmsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"poi/atms.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)greyhoundsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"poi/greyhounds.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)helplinesWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"poi/helplines.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)librariesWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"poi/libraries.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)photospheresWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"poi/photospheres.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)defibrillatorsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"poi/defibrillators.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)constructionsitesWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"poi/constructionsites.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)accessibleEntrancesWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"poi/accessibleentrances.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)visitorInformationWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"poi/visitorinformation.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
 @end
