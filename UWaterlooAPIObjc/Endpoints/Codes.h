edd84ce67eeb32801a973d4aedb9d3f32b039006
Dalton Sweeney <daltyboy11@gmail.com>
2018-07-22 11:33:49 -0400

    codes

--- UWaterlooAPIObjc/Endpoints/Codes.h
+++ UWaterlooAPIObjc/Endpoints/Codes.h
@@ -10,4 +10,19 @@
 
 @interface Codes : Endpoint
 
+/** This method returns a list of all code lookups and their respective descriptions for organizations. */
+- (void)codesByUnitWithCompletion:(Handler)completion;
+
+/** This method returns a list of all code lookups for terms. */
+- (void)codesByTermWithCompletion:(Handler)completion;
+
+/** This method returns a list of all code lookups for groups. */
+- (void)codesByGroupWithCompletion:(Handler)completion;
+
+/** This method returns a list of all code lookups for subjects. */
+- (void)codesBySubjectWithCompletion:(Handler)completion;
+
+/** This method returns a list of Instructions. */
+- (void)instructionsWithCompletion:(Handler)completion;
+
 @end
--- UWaterlooAPIObjc/Endpoints/Codes.m
+++ UWaterlooAPIObjc/Endpoints/Codes.m
@@ -10,4 +10,34 @@
 
 @implementation Codes
 
+- (void)codesByUnitWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"codes/units.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)codesByTermWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"codes/terms.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)codesByGroupWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"codes/groups.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)codesBySubjectWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"codes/subjects.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)instructionsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"codes/instructions.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
 @end
