3267543e1726e56819ca2fc46e94e750d8ace10f
Dalton Sweeney <daltyboy11@gmail.com>
2018-07-22 19:37:51 -0400

    course tests

---
+++ UWaterlooAPIObjcTests/CourseTests.m
@@ -0,0 +1,123 @@
+//
+//  CourseTests.m
+//  UWaterlooAPIObjcTests
+//
+//  Created by Dalton Sweeney on 2018-07-22.
+//  Copyright © 2018 Dalton Sweeney. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "UWAPI.h"
+#import "Course.h"
+
+@interface CourseTests : XCTestCase
+@property (strong) UWAPI* api;
+@end
+
+@implementation CourseTests
+
+- (void)setUp {
+    [super setUp];
+    
+    _api = [[UWAPI alloc] initWithApiKey:@"234279968e219cf1f180a48bf217c318"];
+}
+
+- (void)tearDown {
+    _api = nil;
+    
+    [super tearDown];
+}
+
+- (void)testCourses
+{
+    XCTestExpectation* expectation = [[XCTestExpectation alloc] initWithDescription:@""];
+    [[_api course] coursesWithCompletion:^(NSDictionary * _Nullable dict, NSError * _Nullable error) {
+        XCTAssertNotNil(dict);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    [self waitForExpectations:@[expectation] timeout:1.0];
+}
+
+- (void)testCoursesForSubject
+{
+    XCTestExpectation* expectation = [[XCTestExpectation alloc] initWithDescription:@""];
+    [[_api course] coursesForSubject:@"MATH" withCompletion:^(NSDictionary * _Nullable dict, NSError * _Nullable error) {
+        XCTAssertNotNil(dict);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    [self waitForExpectations:@[expectation] timeout:1.0];
+}
+
+- (void)testCoursesForCourseId
+{
+    XCTestExpectation* expectation = [[XCTestExpectation alloc] initWithDescription:@""];
+    [[_api course] courseForCourseId:@"7407" withCompletion:^(NSDictionary * _Nullable dict, NSError * _Nullable error) {
+        XCTAssertNotNil(dict);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    [self waitForExpectations:@[expectation] timeout:1.0];
+}
+
+- (void)testCourseScheduleForClassNumber
+{
+    XCTestExpectation* expectation = [[XCTestExpectation alloc] initWithDescription:@""];
+    [[_api course] courseScheduleForClassNumber:5377 withCompletion:^(NSDictionary * _Nullable dict, NSError * _Nullable error) {
+        XCTAssertNotNil(dict);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    [self waitForExpectations:@[expectation] timeout:1.0];
+}
+
+- (void)testCourseInfoForSubjectAndCatalog
+{
+    XCTestExpectation* expectation = [[XCTestExpectation alloc] initWithDescription:@""];
+    [[_api course] courseInfoForCourseWithSubject:@"PHYS" andCatalogNumber:@"234" withCompletion:^(NSDictionary * _Nullable dict, NSError * _Nullable error) {
+        XCTAssertNotNil(dict);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    [self waitForExpectations:@[expectation] timeout:1.0];
+}
+
+- (void)testCourseScheduleForSubjectAndCatalog
+{
+    XCTestExpectation* expectation = [[XCTestExpectation alloc] initWithDescription:@""];
+    [[_api course] courseScheduleForSubject:@"CS" andCatalogNumber:@"246" withCompletion:^(NSDictionary * _Nullable dict, NSError * _Nullable error) {
+        XCTAssertNotNil(dict);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    [self waitForExpectations:@[expectation] timeout:1.0];
+}
+
+- (void)testCoursePrerequisitesForSubjectAndCatalog
+{
+    XCTestExpectation* expectation = [[XCTestExpectation alloc] initWithDescription:@""];
+    [[_api course] coursePrerequisitesForSubject:@"CS"
+                                andCatalogNumber:@"246"
+                                  withCompletion:^(NSDictionary * _Nullable dict, NSError * _Nullable error) {
+                                      XCTAssertNotNil(dict);
+                                      XCTAssertNil(error);
+                                      [expectation fulfill];
+    }];
+    [self waitForExpectations:@[expectation] timeout:1.0];
+}
+
+- (void)testCourseExamScheduleForSubjectAndCatalog
+{
+    XCTestExpectation* expectation = [[XCTestExpectation alloc] initWithDescription:@""];
+    [[_api course] courseExamScheduleForSubject:@"CS"
+                               andCatalogNumber:@"246"
+                                 withCompletion:^(NSDictionary * _Nullable dict, NSError * _Nullable error) {
+                                     XCTAssertNotNil(dict);
+                                     XCTAssertNil(error);
+                                     [expectation fulfill];
+    }];
+    [self waitForExpectations:@[expectation] timeout:1.0];
+}
+
+@end
