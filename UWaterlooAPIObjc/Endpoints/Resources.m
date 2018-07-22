1eca93395c8df2ac0c1a988938adb8888537e86f
Dalton Sweeney <daltyboy11@gmail.com>
2018-07-22 11:26:46 -0400

    resources

--- UWaterlooAPIObjc/Endpoints/Resources.m
+++ UWaterlooAPIObjc/Endpoints/Resources.m
@@ -10,4 +10,28 @@
 
 @implementation Resources
 
+- (void)tutorsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"resources/tutors.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)coopInfoSessionsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"resources/infosessions.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)geeseNestsWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"resources/goosewatch.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
+- (void)sunshineListWithCompletion:(Handler)completion
+{
+    NSString* path = [NSString stringWithFormat:@"resources/sunshinelist.%@", [self stringFromFormat:self.responseFormat]];
+    [self getAtPath:path withCompletion:completion];
+}
+
 @end
