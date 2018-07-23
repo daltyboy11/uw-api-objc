3b36bda02ecb7a633e68de7161c1f6bc2edd2cab
Dalton Sweeney <daltyboy11@gmail.com>
2018-07-23 07:49:21 -0400

    news tests

---
+++ UWaterlooAPIObjcTests/NewsTests.m
@@ -0,0 +1,66 @@
+//
+//  NewsTests.m
+//  UWaterlooAPIObjcTests
+//
+//  Created by Dalton Sweeney on 2018-07-23.
+//  Copyright © 2018 Dalton Sweeney. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "UWAPI.h"
+#import "News.h"
+
+@interface NewsTests : XCTestCase
+@property (strong) UWAPI* api;
+@end
+
+@implementation NewsTests
+
+// MARK: - Setup
+- (void)setUp {
+    [super setUp];
+    
+    _api = [[UWAPI alloc] initWithApiKey:@"234279968e219cf1f180a48bf217c318"];
+}
+
+- (void)tearDown {
+    _api = nil;
+    
+    [super tearDown];
+}
+
+// MARK: - Tests
+- (void)testNews
+{
+    XCTestExpectation* expectation = [[XCTestExpectation alloc] initWithDescription:@""];
+    [[_api news] newsWithCompletion:^(NSDictionary * _Nullable dict, NSError * _Nullable error) {
+        XCTAssertNotNil(dict);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    [self waitForExpectations:@[expectation] timeout:1.0];
+}
+
+- (void)testNewsForSite
+{
+    XCTestExpectation* expectation = [[XCTestExpectation alloc] initWithDescription:@""];
+    [[_api news] newsForSite:@"engineering" withCompletion:^(NSDictionary * _Nullable dict, NSError * _Nullable error) {
+        XCTAssertNotNil(dict);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    [self waitForExpectations:@[expectation] timeout:1.0];
+}
+
+- (void)testNewsForSiteAndId
+{
+    XCTestExpectation* expectation = [[XCTestExpectation alloc] initWithDescription:@""];
+    [[_api news] newsForSite:@"science" andId:@"881" withCompletion:^(NSDictionary * _Nullable dict, NSError * _Nullable error) {
+        XCTAssertNotNil(dict);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    [self waitForExpectations:@[expectation] timeout:1.0];
+}
+
+@end
